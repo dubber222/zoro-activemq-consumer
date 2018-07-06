31e4ccac889309b558a4f81971d9e8bb1682accc
dubber222 <17521010890@163.com>
2018-07-06 17:47:46 +0800

    ActiveMQ 广播

--- src/main/java/com/zoro/JmsPersistenceTopicConsumer.java
+++ src/main/java/com/zoro/JmsPersistenceTopicConsumer.java
@@ -8,35 +8,41 @@ import javax.jms.*;
  * Created on 2018/7/5.
  *
  * @author dubber
+ *
+ * 订阅持久化
  */
-public class JmsActivemqConsumer {
+public class JmsPersistenceTopicConsumer {
     private static ConnectionFactory factory;
     private static final String brokeUrl = "tcp://192.168.116.12:61616";
+    private static final String CLIENT_ID = "zoro-1";
     static{
         factory = new ActiveMQConnectionFactory(brokeUrl);
     }
 
     public static void main(String[] args) {
-        new JmsActivemqConsumer().process();
+        new JmsPersistenceTopicConsumer().process();
     }
 
     public void process(){
         Connection conn = null;
         try {
             conn = factory.createConnection();
+            conn.setClientID(JmsPersistenceTopicConsumer.CLIENT_ID);
             conn.start();
             // Boolean.FALSE 非事务操作，  Session.AUTO_ACKNOWLEDGE
-            Session session = conn.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+            Session session = conn.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
 
-            // 创建目的地 queue（队列）
-            Destination destination = session.createQueue("zoro_Queue01");
-            // 创建发送者
-            MessageConsumer consumer = session.createConsumer(destination);
-            // 创建发送的信息
+            // 创建目的地 topic（广播）
+            Topic destination = session.createTopic("zoro_topic01");
+
+            // 创建持久化接收者
+            MessageConsumer consumer = session.createDurableSubscriber(destination,JmsPersistenceTopicConsumer.CLIENT_ID);
+
             TextMessage message = (TextMessage) consumer.receive();
 
-            System.out.println("收到的消息: " +  message.getText());
+            System.out.println("收到的广播: " +  message.getText());
 
+            //session.commit();
             session.close();
         } catch (JMSException e) {
             e.printStackTrace();
---
+++ src/main/java/com/zoro/JmsQueueConsumer2.java
@@ -0,0 +1,54 @@
+package com.zoro;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created on 2018/7/5.
+ *
+ * @author dubber
+ */
+public class JmsQueueConsumer2 {
+    private static ConnectionFactory factory;
+    private static final String brokeUrl = "tcp://192.168.116.12:61616";
+    static{
+        factory = new ActiveMQConnectionFactory(brokeUrl);
+    }
+
+    public static void main(String[] args) {
+        new JmsQueueConsumer2().process();
+    }
+
+    public void process(){
+        Connection conn = null;
+        try {
+            conn = factory.createConnection();
+            conn.start();
+            // Boolean.FALSE 非事务操作，  Session.AUTO_ACKNOWLEDGE
+            Session session = conn.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+
+            // 创建目的地 queue（队列）
+            Destination destination = session.createQueue("zoro_Queue01");
+            // 创建消费者
+            MessageConsumer consumer = session.createConsumer(destination);
+            // 接收信息
+            TextMessage message = (TextMessage) consumer.receive();
+
+            System.out.println("收到的消息: " +  message.getText());
+
+            session.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }finally {
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
